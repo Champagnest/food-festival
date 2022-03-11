e781a37f5ed6ace7a68ba52beb2dcd7937cd5cb5
Champagnest <mchampagned@protonmail.com>
2022-03-11 12:10:30 -0800

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,21 @@
-const path = require('path');
-const webpack = require("webpack");
-const { webpack } = require('webpack');
+const webpack = require("webpack")
+const path = require("path");
+
 module.exports = {
-  entry: './assets/js/script.js',
+  entry: "./assets/js/script.js",
+  events: "./assets/js/events.js",
+  schedule: "./assets/js/schedule.js",
+  tickets: "./assets/js/tickets.js",
+
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.bundle.js'
+    path: __dirname + "/dist",
+    filename: "[name].bundle.js"
   },
-  plugins:[
-    new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery"
-    }),
+  plugins: [
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery"
+  }),
   ],
-  mode: 'development'
+  mode: "development"
 };
