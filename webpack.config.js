f1658046fa86876caf602d6d69395ee250da3dd4
Champagnest <mchampagned@protonmail.com>
2022-03-11 14:40:33 -0800

    webpack improvements

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,47 @@
-const webpack = require("webpack")
+const webpack = require('webpack');
 const path = require("path");
 
-module.exports = {
-  entry: "./assets/js/script.js",
-  events: "./assets/js/events.js",
-  schedule: "./assets/js/schedule.js",
-  tickets: "./assets/js/tickets.js",
 
-  output: {
-    path: __dirname + "/dist",
-    filename: "[name].bundle.js"
-  },
-  plugins: [
-  new webpack.ProvidePlugin({
-    $: "jquery",
-    jQuery: "jquery"
-  }),
-  ],
-  mode: "development"
+module.exports = {
+    entry: {
+        app: './assets/js/script.js',
+        events: './assets/js/events.js',
+        schedule: './assets/js/schedule.js',
+        tickets: './assets/js/tickets.js'
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: __dirname + "/dist"
+      },
+      module: {
+        rules: [
+            {
+              test: /\.jpg$/i,
+              use: [
+                {
+                  loader: 'file-loader',
+                  options: {
+                      esModule: false,
+                      name (file) {
+                          return "[path][name].[ext]"
+                      },
+                      publicPath: function(url) {
+                          return url.replace("../",'/assets');
+                      }
+                  }
+                },
+                {
+                    loader: 'image-webpack-loader'
+                }
+              ]
+            }
+          ]
+        },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        }),
+    ],
+    mode: 'development'
 };
